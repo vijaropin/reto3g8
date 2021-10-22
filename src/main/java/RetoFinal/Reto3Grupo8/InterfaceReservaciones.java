eb48a40f022ca58693904a379d4e75cda73b42d8
vijaropin <vijaro-4@hotmail.com>
2021-10-21 22:52:02 -0500

    first commit

---
+++ src/main/java/RetoFinal/Reto3Grupo8/InterfaceReservaciones.java
@@ -0,0 +1,11 @@
+
+package RetoFinal.Reto3Grupo8;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author PEDRO
+ */
+public interface InterfaceReservaciones extends CrudRepository<Reservaciones, Integer> {
+    
+}
