24023eab4adcbce57e689f0c02c37becd30e9a13
salomonb <salomonestebanbahamon@gmail.com>
2021-11-01 18:28:34 -0500

    agregando
    agregando los metodos para la api

--- src/main/java/RetoFinal/Reto3Grupo8/ServiciosReservaciones.java
+++ src/main/java/RetoFinal/Reto3Grupo8/ServiciosReservaciones.java
@@ -2,17 +2,19 @@
 package RetoFinal.Reto3Grupo8;
 
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-/**
- *
- * @author USUARIO
- */
+
 @Service
 public class ServiciosReservaciones {
+    
     @Autowired
     private RepositorioReservaciones metodosCrud;
 
@@ -68,5 +70,33 @@ public class ServiciosReservaciones {
         }).orElse(false);
         return aBoolean;
     }
+    public StatusReservas 
+reporteStatusServicio (){
+        List<Reservaciones>completed= metodosCrud.ReservacionStatusRepositorio("completed");
+        List<Reservaciones>cancelled= metodosCrud.ReservacionStatusRepositorio("cancelled");
+        
+        return new StatusReservas(completed.size(), cancelled.size() );
+    }
     
+public List<Reservaciones> reporteTiempoServicio (String datoA, String datoB) throws java.text.ParseException{
+        SimpleDateFormat parser = new SimpleDateFormat ("yyyy-MM-dd");
+        
+        Date datoUno = new Date();
+        Date datoDos = new Date();
+        
+        try{
+             datoUno = parser.parse(datoA);
+             datoDos = parser.parse(datoB);
+        }catch(ParseException evt){
+            evt.printStackTrace();
+        }if(datoUno.before(datoDos)){
+            return metodosCrud.ReservacionTiempoRepositorio(datoUno, datoDos);
+        }else{
+            return new ArrayList<>();
+        
+        } 
+    }
+public List<ContadorClientes> reporteClientesServicio(){
+            return metodosCrud.getClientesRepositorio();
+        }
 }
